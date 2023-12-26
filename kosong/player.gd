57f154a49e2bc9e90e4d0fb9f04a46f2164cb318
Farisfanani25 <fananifaris25@gmail.com>
2023-12-26 21:20:18 +0700

    second commit

--- kosong/player.gd
+++ kosong/player.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY =-600.0
+const JUMP_VELOCITY = -700.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,14 +18,16 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			$AnimatedSprite2D.play("kanan")
+			$AnimatedSprite2D.flip_h = false  # Tidak membalik sprite
+		else:
+			$AnimatedSprite2D.play("kiri")
+			$AnimatedSprite2D.flip_h = true  # Membalik sprite
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
-	if Input.is_action_just_pressed("ui_right"):
-		$AnimatedSprite2D.play("kanan")
-		
-	if Input.is_action_just_pressed("ui_left"):
-		$AnimatedSprite2D.play("kiri")
